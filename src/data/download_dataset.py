1cb8e614c7b3b5304df808d4b0dc27f375737ebc
henrique vieira <henrique.vieira@usp.br>
2023-06-06 14:17:02 -0300

    fix(gitignore) remove src/data from gitignore file

---
+++ src/data/download_dataset.py
@@ -0,0 +1,14 @@
+import os
+import urllib.request
+
+class Download():
+
+    def __init__(self, destination_path):
+        self.destination_path = destination_path
+
+    def download(self):
+
+        path = self.destination_path
+        response = urllib.request.urlretrieve("https://www.opennn.net/files/breast_cancer.csv", os.path.join(path, "breast_cancer.csv"))
+
+        return response
